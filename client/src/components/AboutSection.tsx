88dfd14413fdfb329fba08578b0370fc6e439b11
navamc1987 <128137083+navamc1987@users.noreply.github.com>
2026-03-18 12:57:44 +0700

    Update AboutSection.tsx

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -110,8 +110,8 @@ export default function AboutSection() {
                 </div>
                 </div>
                 <div className="flex gap-3">
-                  <span className="text-[#C9A84C] font-semibold w-28 shrink-0">Line ID</span>
-                  <span className="text-white/70">0900072977</span>
+                  <span className="text-[#C9A84C] font-semibold w-28 shrink-0">โทรเลย</span>
+                  <span className="text-white/70">081-079-3266</span>
               </div>
             </div>
 
